ca4ea12bf06838fb65f387709bb779617a1f5d0b
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-12-04 15:39:28 +0000

    Updated input ranges at script.js

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ function getNumberInfo() {
     // Get the Value of the Input Field
     let num = document.getElementById("mynumber").value;
     // Check if input is valid
-    if (isNaN(num) || num.length == 0 || num < 13 || num > 10000 || (num.length > 1 && num[0] == "0") || !Number.isInteger(Number(num))) {
-        txt += `Invalid Input.  Please enter a whole number between 13 and 10000.  Do not include leading zeros.`;
+    if (isNaN(num) || num.length == 0 || num < 2 || num > 10000 || (num.length > 1 && num[0] == "0") || !Number.isInteger(Number(num))) {
+        txt += `Invalid Input.  Please enter a whole number between 2 and 10000.  Do not include leading zeros.`;
     } else {
         txt += `You have entered the number ${num}. <p>`;
         txt += `Exactly ${oddPeriodSqrts(num)} continued fractions, for N ≤ ${num}, have an odd period.`
